aa911908aad6f079b99d861f2c24c8751dcd9db9
moliveros <mcoliveros1202@gmail.com>
2022-05-11 17:14:00 -0500

    set if else statements to set up fighting

--- assets/js/game.js
+++ assets/js/game.js
@@ -17,15 +17,29 @@ var fight = function() {
   enemyHealth = enemyHealth - playerAttack;
 
   //Log a resulting message to the console so we know that it worked.
-  console.log(playerName + "attacked" + enemyName + "." + enemyName + "now has" + enemyHealth + "health remaining.");
+  console.log(
+    playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
 
   //Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the value in the 'playerHealth' variable.
   playerHealth = playerHealth - enemyAttack;
   
   //Log a resulting message to the console so we know that it worked.
   console.log(
-    playerName + "attacked" + enemyName + ". " + enemyName + "now has" + enemyHealth + " health remaining."
+    enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
   )
 };
 
 fight();
+// put new code under this
+console.log(
+  playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+);
+
+//check enemy's health
+if (enemyHealth <= 0) {
+  window.alert(enemyName + " has died!");
+}
+else {
+  window.alert(enemyName + " still has " + enemyHealth + " health left.");
+
+}
