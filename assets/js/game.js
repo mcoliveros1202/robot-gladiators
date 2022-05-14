fec1c5930664be58d4d70260a33b041fa6f4b886
Moogs <mcoliveros1202@gmail.com>
2022-05-14 17:15:40 -0500

    Random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -26,17 +26,16 @@ var fight = function (enemyName) {
       if (confirmSkip) {
         window.alert(playerName + " has decide to skip this fight. Goodbye!");
         // subtract money from playerMoney for skipping
-        playerMoney = playerMoney - 10;
+        playerMoney = Math.max(0, playerMoney - 10);
         console.log("playerMoney", playerMoney);
         break;
       }
     }
 
-    // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
-    console.log(
-      playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
-    );
+    // generate random damage value baed on player's attack power
+    var damage = randomNumber(playerAttack - 3, playerAttack);
+
+    enemyHealth = Math.max(0, enemyHealth - damage);
 
     // check enemy's health
     if (enemyHealth <= 0) {
@@ -52,7 +51,9 @@ var fight = function (enemyName) {
     }
 
     // remove player's health by subtracting the amount set in the enemy Attack variable
-    playerHealth = playerHealth - enemyAttack;
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+    playerHealth = Math.max(0, enemyHealth - damage);
     console.log(
       enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
     );
@@ -85,7 +86,7 @@ var startGame = function () {
       var pickedEnemyName = enemyNames[i];
 
       // reset enemyHealth before starting new fight
-      enemyHealth = 50;
+      enemyHealth = randomNumber(40, 60);
 
       //use debugger to pause script from running and check what's going on at the moment in the code
       // debugger;
@@ -111,6 +112,12 @@ var startGame = function () {
   }
 };
 
+//function to generate a random numeric value
+var randomNumber = function() {
+  var value = Math.floor(Math.random() * (21) + 40);
+
+  return value;
+};
 // start the game when the page loads
 var shop = function () {
    // ask player what they would like to do
@@ -172,9 +179,9 @@ var endGame = function () {
 
 endGame();
 
-startGame();
+//startGame();
 
-// provide access to a shop for player to purchase HEALTH, ATTACK, or to leave shop
-// at the end of each round (if there are more enemies), provide opportunity for player to go to shop-- if yes, shop() function -- if no, continue to next round.
-// prompt with window.prompt what they would like to do: purchase health REFILL (subtact money and add health points), UPGRADE attack (subtact money, add attack power), or LEAVE shop (alert goodbye and start fight() function); if an invalid option is selected, call shop() fn again.
-// from shop, start next round of fighting
+// use Math object to add randomness to the game
+// merge and switch branches
+// convert player and enemy data to custom objects
+// merge the object branch and update main
